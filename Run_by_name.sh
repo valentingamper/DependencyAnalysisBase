7b1ec39d21c9c7bf98145e944ebcb000f5f506e1
Konstiu <154671504+Konstiu@users.noreply.github.com>
2025-06-08 14:07:06 +0200

    Update Run_by_name.sh

--- Run_by_name.sh
+++ Run_by_name.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Array mit 7 unterschiedlichen Strings
 STRINGS=(
   "keycloak/keycloak"
   "apache/tomcat"
@@ -10,15 +9,11 @@ STRINGS=(
   "intelowlproject/IntelOwl"
   "yeti-platform/yeti"
   "smicallef/spiderfoot"
-  "SigmaHQ/sigma"
-  "honeynet/honeyscanner"
   "mitre/caldera"
 )
 
-# Pfad zu deinem Python-Skript
 PYTHON_SCRIPT="Dependency_analysis_notebook.py"
 
-# For-Loop über alle Strings
 for s in "${STRINGS[@]}"; do
   echo "Rufe Python-Skript mit Argument: $s auf"
   python "$PYTHON_SCRIPT" "$s"
